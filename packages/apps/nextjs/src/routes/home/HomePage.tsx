156b5b9867ee76a0fd55f2b0ae0c0089e3d5f6a4
Ryan Bower <ryanbower01@gmail.com>
2021-05-14 13:56:26 -0700

    fixed compilation errors

--- packages/apps/nextjs/src/routes/home/HomePage.tsx
+++ packages/apps/nextjs/src/routes/home/HomePage.tsx
@@ -8,7 +8,7 @@ import {pagePaddingX, pagePaddingY} from "~components/page/config"
 export default function HomePage(): JSX.Element {
   return (
     <Box height={500} width={500} {...pagePaddingX} {...pagePaddingY}>
-      <TruncatedText as={"h3"} maxLines={["2"]}>
+      <TruncatedText as={"h3"} maxLines={[2]}>
         Sed ut perspiciatis unde omnis iste natus error sit voluptatem
         accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
         illo inventore veritatis et quasi architecto beatae vitae dicta sunt
