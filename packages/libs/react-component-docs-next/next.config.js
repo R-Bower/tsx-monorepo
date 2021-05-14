1ba6d3b3eb934300fc5c68f8c974327cbea7509c
Ryan Bower <ryanbower01@gmail.com>
2021-05-14 11:12:29 -0700

    working gatsby docs

---
+++ packages/libs/react-component-docs-next/next.config.js
@@ -0,0 +1,25 @@
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin")
+
+module.exports = {
+  future: {
+    webpack5: false,
+  },
+  reactStrictMode: true,
+  webpack: (config, options) => {
+    // Let Babel compile outside of src/.
+    const tsRule = config.module.rules.find(
+      (rule) => rule.test && rule.test.toString().includes("tsx|ts"),
+    )
+    tsRule.include = undefined
+    tsRule.exclude = /node_modules/
+
+    const {dev, isServer} = options
+
+    // Do not run type checking twice:
+    if (dev && isServer) {
+      config.plugins.push(new ForkTsCheckerWebpackPlugin())
+    }
+
+    return config
+  },
+}
